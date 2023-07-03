f9d93f7c9f329d2d971be5be4fae5d327bcb254b
shufps <shufps80@gmail.com>
2023-07-03 12:42:45 +0200

    load networks.config dynamically

--- src/lib/evm-toolkit/constants/index.ts
+++ src/lib/evm-toolkit/constants/index.ts
@@ -1,1 +1,1 @@
-export * from './networks.constant'
+
--- src/lib/evm-toolkit/stores/network.stores.ts
+++ src/lib/evm-toolkit/stores/network.stores.ts
@@ -1,21 +1,28 @@
-import { derived, type Readable, type Writable } from 'svelte/store';
-
+import { derived, writable, get } from 'svelte/store';
 import { persistent } from '$lib/stores';
-
-import { NETWORKS } from '../constants';
 import type { INetwork } from '../interfaces';
 
 const SELECTED_NETWORK_KEY = 'selectedNetworkId';
-const NETWORKS_KEY = 'networks';
 
-export const networks: Writable<INetwork[]> = persistent<INetwork[]>(NETWORKS_KEY, NETWORKS);
+export const networks = writable<INetwork[]>([]);
 export const selectedNetworkId: Writable<number> = persistent(
     SELECTED_NETWORK_KEY,
     0,
 );
 
+// Fetch the networks data at runtime
+export async function fetchNetworksData() {
+    const response = await fetch('/networks.json');  // adjust the path as necessary
+    if (!response.ok) {
+        throw new Error("Networks data fetch failed");
+    }
+    const data = await response.json();
+    networks.set(data);
+}
+
+// Use derived store as before, but make sure you have called fetchNetworksData() beforehand
 export const selectedNetwork: Readable<INetwork> = derived(
-    ([networks, selectedNetworkId]), ([$networks, $selectedNetworkId]) => {
+    [networks, selectedNetworkId], ([$networks, $selectedNetworkId]) => {
         if (!$networks?.length || !($selectedNetworkId >= 0)) {
             return null;
         }
