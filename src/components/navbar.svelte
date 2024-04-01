aecad4c11dca99798347a1b4587cf15554df8b37
evavirseda <evirseda@boxfish.studio>
2024-04-01 16:55:15 +0200

    feat: update assets theme

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -8,6 +8,8 @@
   import { PopupId } from '$lib/popup';
   import { openPopup } from '$lib/popup/actions';
   import { connectToWallet } from '$lib/withdraw';
+    import { appConfiguration } from '$lib/evm-toolkit';
+    import SettingsIcon from './settings-icon.svelte';
 
   function handleSettings() {
     openPopup(PopupId.Settings);
@@ -35,14 +37,11 @@
 
 <nav class="flex justify-between items center">
   <image-wrapper class="h-full flex items-center">
-    <img src="/logo.svg" alt="Shimmer logo" />
-    <h1 class="text-md md:text-2xl ml-4 text-white font-semibold">shimmer</h1>
+    <img src="/logo-{$appConfiguration?.theme}.svg" alt="{$appConfiguration?.theme} logo" />
+    <h1 class="text-md md:text-2xl ml-4 text-text-default font-semibold">{$appConfiguration?.theme}</h1>
   </image-wrapper>
   <items-wrapper class="flex items-center space-x-4 mr-4">
-    <img
-      src="/settings-icon.svg"
-      alt="Shimmer logo"
-      class="cursor-pointer p-2"
+    <SettingsIcon
       on:click={handleSettings}
       on:keydown={event => handleEnterKeyDown(event, handleSettings)}
     />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 
   import { Navbar, NotificationManager, PopupManager } from '$components';
 
-  import { fetchConfiguredNetworks, networks } from '$lib/evm-toolkit';
+  import { Theme, appConfiguration, fetchConfiguredNetworks, networks } from '$lib/evm-toolkit';
 
   import '../app.scss';
 
@@ -29,7 +29,7 @@
 <Navbar />
 <main class="w-full flex flex-1 items-center justify-center">
   <background-decorator>
-    <div />
+    <div style={$appConfiguration?.theme === Theme.Shimmer ? "background-image: url('/bg-shimmer-shapes.svg');": "background-image: url('/bg-iota-shapes.svg');"} />
   </background-decorator>
   <slot />
 </main>
@@ -42,14 +42,13 @@
     min-height: calc(100vh - (var(--navbar-height)));
   }
   background-decorator {
-    @apply bg-shimmer-background;
+    @apply bg-background-primary;
     @apply w-screen h-screen fixed z-[-1] top-0 left-0;
 
     div {
       @apply w-screen h-screen;
       @apply bg-no-repeat bg-cover;
       background-position: 50% center;
-      background-image: url('/bg-shapes.svg');
     }
   }
 </style>
